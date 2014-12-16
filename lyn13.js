1567317ec52ee0b6a6bbd335c3f081a5ad3b1c94
Dominik Hiltbrunner <nikhilti@hispeed.ch>
2014-12-16 14:19:29 +0100

    13 done

--- lyn13.js
+++ lyn13.js
@@ -3,19 +3,21 @@ var url = require('url');
 
 var port = process.argv[2];
 
-var server = http.createServer(function (req, res) {
-
+http.createServer(function(req, res) {
   var reqUrl = url.parse(req.url, true);
   if(reqUrl.pathname == '/api/parsetime') {
-    var iso = reqUrl.query.iso;
+    var isoDate = new Date(reqUrl.query.iso);
+	var printDate = { 'hour': isoDate.getHours(), 'minute': isoDate.getMinutes(), 'second':isoDate.getSeconds()};
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write('bla');
+    res.end(JSON.stringify(printDate));
   }
   else if(reqUrl.pathname == '/api/unixtime') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
+	var isoDate = new Date(reqUrl.query.iso);
+    res.end(JSON.stringify({'unixtime': isoDate.getTime()}));
   }
   else {
-    //404
+    res.writeHead(404);
+    res.end();
   }
-
 }).listen(port);
